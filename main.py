4a615d9ca9178f9c02a8cbb7c6c241005fd6eaf3
Fırat Batar <firat.o.batar@gmail.com>
2023-04-23 14:37:15 +0300

    Removed singles from main function. Now can run multiple runs with different parameters each.

--- main.py
+++ main.py
@@ -38,7 +38,10 @@ def select_singles(sock_count: int, usage_probability: float, max_cycle: int, ru
     return [sock_ages, age_count]
 
 
-def main(pairs: bool = True, singles: bool = False, save: bool = False, parameters: dict = None, max_run: int = 1):
+def main(save: bool = False, parameters: dict = None, max_run: int = 1):
+    if len(parameters) != max_run:
+        raise ValueError("Parameters do not match with the number of runs")
+
     from openpyxl import Workbook
     from openpyxl.worksheet.table import Table, TableStyleInfo
     from time import time
@@ -48,100 +51,51 @@ def main(pairs: bool = True, singles: bool = False, save: bool = False, paramete
     FILE_PATH = "data/"
     FILE_END = "-raw_data.xlsx"
 
-    # Parameters
-    if parameters is None:
-        # Parameters as inputs
-        SOCK_COUNT = int(input("Enter total number of pairs: ")) * 2
-        if SOCK_COUNT <= 0:
-            print("Invalid input")
-            return
-        USAGE_PROBABILITY = float(input("Enter the usage probability of a pair (between 0 and 1): "))
-        if not (0 <= USAGE_PROBABILITY < 1):
-            print("Invalid input")
-            return
-        MAX_CYCLE = int(input("Enter total number of cycles: "))
-        if MAX_CYCLE <= 0:
-            print("Invalid input")
-            return
-    else:
-        # Predefined parameters
-        SOCK_COUNT = parameters["SOCK_COUNT"]
-        USAGE_PROBABILITY = parameters["USAGE_PROBABILITY"]
-        MAX_CYCLE = parameters["MAX_CYCLE"]
-
     workbook = worksheet = None
-    if pairs:
+    SOCK_COUNT_OLD = 0
+    if save:
+        # Workbook for raw data
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.title = "Data"
+
+    for run in range(max_run):
+        # Parameters
+        SOCK_COUNT = parameters[f"P{run + 1}"]["SOCK_COUNT"]
+        USAGE_PROBABILITY = parameters[f"P{run + 1}"]["USAGE_PROBABILITY"]
+        MAX_CYCLE = parameters[f"P{run + 1}"]["MAX_CYCLE"]
+
+        # Get the age count for selecting pairs
+        [sock_ages, age_count] = select_pairs(SOCK_COUNT, USAGE_PROBABILITY, MAX_CYCLE, run + 1)
+
         if save:
-            # Workbook for raw data
-            workbook = Workbook()
-            worksheet = workbook.active
-            worksheet.title = "Data"
+            # Data column titles
+            worksheet.cell(row=SOCK_COUNT_OLD + 1, column=1, value="Sock")
+            worksheet.cell(row=SOCK_COUNT_OLD + 1, column=2, value=f"Age#{run + 1}")
 
-        for run in range(max_run):
-            # Get the age count for selecting pairs
-            [sock_ages, age_count] = select_pairs(SOCK_COUNT, USAGE_PROBABILITY, MAX_CYCLE, run + 1)
+            # Save the data
+            save_data(worksheet, sock_ages, 1, SOCK_COUNT_OLD + 2)
 
-            if save:
-                # Data column titles
-                worksheet.cell(row=(SOCK_COUNT + 2) * run + 1, column=1, value="Sock")
-                worksheet.cell(row=(SOCK_COUNT + 2) * run + 1, column=2, value="Age")
+            # Create the table
+            tab = Table(displayName=f"PairsTable{run + 1}", ref=f"A{SOCK_COUNT_OLD + 1}"
+                                                                f":B{SOCK_COUNT_OLD + SOCK_COUNT + 1}")
+            # Add a default style with striped rows and banded columns
+            style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                                   showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+            tab.tableStyleInfo = style  # Apply the style
+            # Add table to the worksheet
+            worksheet.add_table(tab)
 
-                # Save the data
-                save_data(worksheet, sock_ages, 1, (SOCK_COUNT + 2) * run + 2)
-                # Create the table
-                tab = Table(displayName=f"PairsTable{run + 1}", ref=f"A{(SOCK_COUNT + 2) * run + 1}"
-                                                                    f":B{(SOCK_COUNT + 2) * (run + 1) - 1}")
+        else:
+            print(sock_ages)
 
-                # Add a default style with striped rows and banded columns
-                style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
-                                       showLastColumn=False, showRowStripes=True, showColumnStripes=True)
-                tab.tableStyleInfo = style  # Apply the style
+        SOCK_COUNT_OLD += SOCK_COUNT + 2
 
-                # Add table to the worksheet
-                worksheet.add_table(tab)
-                # Save the files with the ending 'FILE_END'
-                workbook.save(FILE_PATH + "selecting_pairs" + FILE_END)
+    if save:
+        # Save the files with the ending 'FILE_END'
+        workbook.save(FILE_PATH + "selecting_pairs" + FILE_END)
 
-                print(f"The data of 'selecting pair' was saved to '{FILE_PATH}selecting_pairs{FILE_END}'.\n\n")
-            else:
-                print(sock_ages)
-
-    if singles:
-        if save:
-            # Workbook for raw data
-            workbook = Workbook()
-            worksheet = workbook.active
-            worksheet.title = "Data"
-
-        for run in range(max_run):
-            # Get the age count for selecting pairs
-            [sock_ages, age_count] = select_singles(SOCK_COUNT, USAGE_PROBABILITY, MAX_CYCLE, run + 1)
-
-            if save:
-                # Data column titles
-                worksheet.cell(row=1, column=1, value="Sock")
-                worksheet.cell(row=1, column=2, value="Age")
-
-                # Save the data
-                save_data(worksheet, sock_ages, 1, (SOCK_COUNT + 2) * run + 2)
-                # Create the table
-                tab = Table(displayName=f"SinglesTable{run}", ref=f"A{(SOCK_COUNT + 2) * run + 1}"
-                                                                  f":B{(SOCK_COUNT + 2) * (run + 1) - 1}")
-
-                # Add a default style with striped rows and banded columns
-                style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
-                                       showLastColumn=False, showRowStripes=True, showColumnStripes=True)
-                tab.tableStyleInfo = style  # Apply the style
-
-                # Add table to the worksheet
-                worksheet.add_table(tab)
-
-                # Save the files with the ending 'FILE_END'
-                workbook.save(FILE_PATH + "selecting_singles" + FILE_END)
-
-                print(f"The data of 'selecting singles' was saved to '{FILE_PATH}selecting_singles{FILE_END}'.\n\n")
-            else:
-                print(sock_ages)
+        print(f"The data of 'selecting pair' was saved to '{FILE_PATH}selecting_pairs{FILE_END}'.\n\n")
 
 
 if __name__ == '__main__':
@@ -150,9 +104,21 @@ if __name__ == '__main__':
 
     # Parameters
     parameters = {
-        "SOCK_COUNT": 50,
-        "USAGE_PROBABILITY": 0.2,
-        "MAX_CYCLE": 100,
+        "P1": {
+            "SOCK_COUNT": 50,
+            "USAGE_PROBABILITY": 0.2,
+            "MAX_CYCLE": 100,
+        },
+        "P2": {
+            "SOCK_COUNT": 60,
+            "USAGE_PROBABILITY": 0.2,
+            "MAX_CYCLE": 100,
+        },
+        "P3": {
+            "SOCK_COUNT": 50,
+            "USAGE_PROBABILITY": 0.2,
+            "MAX_CYCLE": 100,
+        }
     }
 
-    main(pairs=True, singles=False, save=True, parameters=parameters, max_run=50)
+    main(save=True, parameters=parameters, max_run=3)
